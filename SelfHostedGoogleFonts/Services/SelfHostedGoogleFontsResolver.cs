4195d788cc813c21ee538cc24eb6f75f44c4f838
David Thibault <david.thibault@sigmund.ca>
2024-02-28 14:37:42 -0500

    Add logging, cleanup sample

--- SelfHostedGoogleFonts/Services/SelfHostedGoogleFontsResolver.cs
+++ SelfHostedGoogleFonts/Services/SelfHostedGoogleFontsResolver.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections.Concurrent;
+using Microsoft.Extensions.Logging;
 using SelfHostedGoogleFonts.Helpers;
 using SelfHostedGoogleFonts.Storage;
 
 namespace SelfHostedGoogleFonts.Services;
 
-public class SelfHostedGoogleFontsResolver(IFontStorage storage, IHttpClientFactory httpClientFactory)
+public class SelfHostedGoogleFontsResolver(
+    IFontStorage storage,
+    IHttpClientFactory httpClientFactory,
+    ILogger<SelfHostedGoogleFontsResolver> logger
+)
 {
     private readonly ConcurrentDictionary<string, string> _cache = new();
     private readonly HttpClient _httpClient = httpClientFactory.CreateClient();
@@ -17,8 +22,22 @@ public class SelfHostedGoogleFontsResolver(IFontStorage storage, IHttpClientFact
         Array.Sort(fontSpecs);
 
         var cacheKey = string.Join("|", fontSpecs);
-        var stylesheetUrl = await _cache.GetOrAddAsync(cacheKey, _ => GetSelfHostedStylesheetUrlInternal(fontSpecs))
+        
+        logger.LogTrace("{Count} specs requested, cache key: {CacheKey}", fontSpecs.Length, cacheKey);
+
+        var fromCache = true;
+        var stylesheetUrl = await _cache.GetOrAddAsync(cacheKey, _ =>
+            {
+                logger.LogTrace("Cache miss: {CacheKey}", cacheKey);
+                fromCache = false;
+                return GetSelfHostedStylesheetUrlInternal(fontSpecs);
+            })
             .ConfigureAwait(false);
+
+        if (fromCache)
+        {
+            logger.LogTrace("Stylesheet for {CacheKey} was found in cache: {Url}", cacheKey, stylesheetUrl);
+        }
 
         return stylesheetUrl;
     }
@@ -28,15 +47,31 @@ public class SelfHostedGoogleFontsResolver(IFontStorage storage, IHttpClientFact
         var sourceUrl = CreateGoogleFontsUrl(sortedFontSpecs);
         var expectedFilename = $"stylesheets/{sourceUrl.Sha256()}.css";
 
+        logger.LogTrace("Source stylesheet URL: {Source} | Expected filename: {Filename}", sourceUrl, expectedFilename);
+
         if (!await storage.FileExistsAsync(expectedFilename).ConfigureAwait(false))
         {
+            logger.LogDebug("{Filename} does not exist in storage, processing", expectedFilename);
+            
+            logger.LogTrace("Downloading source stylesheet from: {Source}", sourceUrl);
             var sourceCss = await _httpClient.GetStringAsync(sourceUrl).ConfigureAwait(false);
+            
+            logger.LogTrace("Processing URLs from source file");
             var processedCss = await CssHelper.ProcessUrlsAsync(
                     sourceCss,
                     async resourceUrl => await GetSelfHostedResourceUrl(resourceUrl))
                 .ConfigureAwait(false);
 
+            logger.LogTrace("Storing processed stylesheet as {Filename}", expectedFilename);
             await storage.StoreStylesheetAsync(expectedFilename, processedCss).ConfigureAwait(false);
+
+            logger.LogInformation(
+                "New stylesheet processed from {Source} and saved to {Filename}", 
+                sourceUrl, expectedFilename);
+        }
+        else
+        {
+            logger.LogDebug("{Filename} found in storage", expectedFilename);
         }
 
         return storage.GetFileUrl(expectedFilename);
@@ -47,14 +82,25 @@ public class SelfHostedGoogleFontsResolver(IFontStorage storage, IHttpClientFact
         var extension = sourceUrl.Split('.').Last();
         var filename = $"fonts/{sourceUrl.Sha256()}.{extension}";
 
+        logger.LogTrace("Source asset URL: {Source} | Expected filename: {Filename}", sourceUrl, filename);
+
         if (!await storage.FileExistsAsync(filename))
         {
+            logger.LogDebug("{Filename} does not exist in storage, downloading", filename);
+            
             var response = await _httpClient.GetAsync(sourceUrl).ConfigureAwait(false);
             var data = await response.Content.ReadAsStreamAsync().ConfigureAwait(false);
             var contentType = response.Content.Headers.ContentType?.MediaType;
             
+            logger.LogTrace("Detected content type: {ContentType}", contentType);
+            
+            logger.LogTrace("Storing asset as {Filename}", filename);
             await storage.StoreAssetAsync(filename, data, contentType).ConfigureAwait(false);
-        } 
+        }
+        else
+        {
+            logger.LogDebug("{Filename} found in storage", filename);
+        }
                             
         return storage.GetFileUrl(filename);
     }
